2f5a4f1ae88bc7c392c431f22067f34fde01e6c2
f4sn <airatsiw@gamil.com>
2013-10-26 00:43:41 +0900

    add many func

--- iNCPush.rb
+++ iNCPush.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
+#!/usr/bin/env ruby
 require './setting'
+require './im_modul'
 require 'digest/sha1'
 require 'net/http'
 require 'json'
@@ -7,52 +9,7 @@ require 'tweetstream'
 require 'twitter'
 require 'pp'
 
-module IM
- class Kayac
-   HOST = 'im.kayac.com'
-   def initialize(to, params={})
-     if to.nil?
-       raise ArgumentError
-     else
-       @to = to
-     end
-     @id  = params[:id]
-     @pwd = params[:password]
-     @sig = params[:sig]
-   end
-
-   def notify(msg,han)
-     if !@sig.nil?
-       sig = Digest::SHA1.hexdigest(msg + @sig)
-       data = 'message=%s&amp;handler=%s;sig=%s' % [u(msg),u(han),sig]
-     elsif !@pwd.nil?
-       data = 'message=%s&amp;password=%s;handler=%s' % [u(msg), u(@pwd),u(han)]
-     else
-       data = 'message=%s;handler=%s' % [u(msg),u(han)]
-     end
-     data << '&amp;id=%s' % [u(@id)] unless @id.nil?
-     header = {
-       'Host' => HOST,
-       'Content-Type' => 'application/x-www-form-urlencoded',
-       'Content-length' => data.size.to_s,
-     }
-
-     path = '/api/post/%s' % [@to]
-     Net::HTTP.version_1_2
-     Net::HTTP.start(HOST, 80) do |http|
-       http.post(path, data, header)
-     end
-   end
-   
-   private
-   def u(str)
-      str.to_s do
-       '%%%02X' % $&.unpack("C").first
-     end
-   end
-
- end
-end
+include IM
 
 
 iNC = IM::Kayac.new(TO_ID,:Id=>MY_ID, :password =>PAS,:sig=>SIG)
@@ -74,7 +31,7 @@ end
 
 
 client = TweetStream::Client.new
-puts "OK"
+puts "Successful connection"
 
   client.on_error do |message|
 end
@@ -90,7 +47,8 @@ client.on_timeline_status  do |status|
   mes_sn=status.user.screen_name
   mes_n=status.user.name
   mes_t=status.text
-  iNC.notify(mes_sn+"\n"+mes_n+"\n"+mes_t,nil)
+  iNC.notify(mes_sn+"\n"+mes_n+"\n"+mes_t,nil) #第2引数にはURLSchemeを指定できます('music:'、'mailto:'、'http://google.com'等)
+  puts "name :#{mes_n}/#{mes_sn}\ntext :#{mes_t}\n\n"
   
 end
 
